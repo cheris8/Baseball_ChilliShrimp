de8639d0268887c531b42455ee78ccd7e02e0cb2
dhxoghks95 <dhxoghks95@gmail.com>
2020-08-20 23:59:13 +0900

    2020년 7월 19일 이전 경기 만들기 추가

--- code/preprocessing/games.R
+++ code/preprocessing/games.R
@@ -18,6 +18,8 @@ game = game %>% select(GDAY_DS, VISIT_KEY, HOME_KEY)
 game = game %>% mutate(GDAY_DS = ymd(GDAY_DS)) %>% mutate(year = year(GDAY_DS), month = month(GDAY_DS)) %>%
   select(year, month, VISIT_KEY, HOME_KEY)
 
+game
+
 game_visit = game %>% mutate(count = 1) %>% group_by(year, month, VISIT_KEY) %>% summarise(VISIT_G = sum(count)) %>% 
   ungroup() %>% mutate(T_ID = VISIT_KEY) %>% select(year, month, T_ID, VISIT_G)
 game_home = game %>% mutate(count = 1) %>% group_by(year, month, HOME_KEY) %>% summarise(HOME_G = sum(count)) %>%
@@ -25,8 +27,20 @@ game_home = game %>% mutate(count = 1) %>% group_by(year, month, HOME_KEY) %>% s
 
 games = game_visit %>% left_join(game_home, by = c('year', 'month', 'T_ID'))
 
+games
+
 df_merged = df %>% left_join(games, by = c('year', 'month', 'T_ID'))
 
 df_merged
 
 write.csv(df_merged, 'games_merged.csv', row.names = F)
+
+
+vs = game %>% filter(year == 2020) %>% mutate(T_ID = VISIT_KEY, VS_T_ID = HOME_KEY) %>% select(year, month, T_ID, VS_T_ID)
+t = game %>% filter(year == 2020) %>% mutate(T_ID = HOME_KEY, VS_T_ID = VISIT_KEY) %>% select(year, month, T_ID, VS_T_ID)
+
+game_2020 = rbind(vs, t) %>% arrange(year, month)
+
+game_2020 %>% View()
+
+write.csv(game_2020, 'game_2020.csv', row.names = F)
