7cd33d7cc1ed211eee8e3699d9569cdf312dbc79
Seungjun1588 <68596054+Seungjun1588@users.noreply.github.com>
2020-08-22 03:19:38 +0900

    2020 타자 각종 비율 수정

--- code/preprocessing/batter_rate_BABIP_2020.R
+++ code/preprocessing/batter_rate_BABIP_2020.R
@@ -5,46 +5,43 @@ names(batter_rate)
 
 data = batter_rate %>%
   filter(year==2020) %>%
-  select(c(P_ID,HIT,H2,H3,HR)) %>%
   group_by(P_ID) %>%
-  summarise(HIT=sum(HIT),H2=sum(H2),H3=sum(H3),HR=sum(HR))
+  summarise(PA=sum(PA),AB=sum(AB), HIT=sum(HIT),H2=sum(H2),H3=sum(H3),HR=sum(HR),SB=sum(SB),CS=sum(CS),
+    BB=sum(BB),HP=sum(HP),KK=sum(KK),GD=sum(GD))
 
 data =data %>%
-  mutate(H2_rate=H2/HIT, H3_rate=H3/HIT, HR_rate=HR/HIT) 
+  mutate(H2_rate=H2/HIT, H3_rate=H3/HIT, HR_rate=HR/HIT,SB_rate=(SB+CS)/PA, SB_SUCC = SB/(SB+CS),
+         BB_rate=BB/PA, HP_rate=HP/PA, KK_rate=KK/PA, GD_rate=GD/PA )
 
 names(data)
 
 data$H2_rate[is.na(data$H2_rate)] = 0
 data$H3_rate[is.na(data$H3_rate)] = 0
 data$HR_rate[is.na(data$HR_rate)] = 0
+data$SB_rate[is.na(data$SB_rate)] = 0
+data$SB_SUCC[is.na(data$SB_SUCC)] = 0
+data$BB_rate[is.na(data$BB_rate)] = 0
+data$HP_rate[is.na(data$HP_rate)] = 0
+data$KK_rate[is.na(data$KK_rate)] = 0
+data$GD_rate[is.na(data$GD_rate)] = 0
+
 
 data$H2_rate[is.infinite(data$H2_rate)] = 0
 data$H3_rate[is.infinite(data$H3_rate)] = 0
 data$HR_rate[is.infinite(data$HR_rate)] = 0
+data$SB_rate[is.infinite(data$SB_rate)] = 0
+data$SB_SUCC[is.infinite(data$SB_SUCC)] = 0
+data$BB_rate[is.infinite(data$BB_rate)] = 0
+data$HP_rate[is.infinite(data$HP_rate)] = 0
+data$KK_rate[is.infinite(data$KK_rate)] = 0
+data$GD_rate[is.infinite(data$GD_rate)] = 0
+
+
+
+
 
 data = data %>%
-  select(c(P_ID,H2_rate,H3_rate,HR_rate))
+  select(c(P_ID,H2_rate,H3_rate,HR_rate,SB_rate,BB_rate,HP_rate,KK_rate,GD_rate))
 
 names(data)
-
-
-BABIP = batter_rate %>%  
-  filter(year==2020) %>%
-  group_by(P_ID) %>%                #  타자 P_ID 별로 통합된 변수들 생성
-  summarise(Total_HIT = sum(HIT),Total_HR = sum(HR),Total_AB = sum(AB),Total_KK = sum(KK),Total_SF = sum(SF)) %>%
-  mutate(BABIP = (Total_HIT - Total_HR) / (Total_AB - Total_KK - Total_HR + Total_SF))   # BABIP 계산
-
-head(BABIP)
-
-BABIP = BABIP %>%
-  select(BABIP)
-sum(is.na(BABIP$BABIP))             
-sum(is.infinite((BABIP$BABIP)))
-
-BABIP$BABIP[is.na(BABIP$BABIP)] = 0
-
-sum(is.na(BABIP$BABIP))
-
-df = cbind(data,BABIP)
-
-write.csv(df,'C:/Users/seungjun/Desktop/baseball/Baseball_ChilliShrimp/data/batter_rate_BABIP_2020.csv')
+write.csv(data,'C:/Users/seungjun/Desktop/baseball/Baseball_ChilliShrimp/data/batter_rate_BABIP_2020.csv')
